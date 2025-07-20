4b0b0ddc1f7c04a711c5c9501a00d390c70a3579
Kai Law <lawkai@vivo.com>
2025-07-20 12:39:03 +0800

    Use spaces rather than tabs (#1)

--- translate.sh
+++ translate.sh
@@ -13,4 +13,4 @@ set -e
 MDBOOK_OUTPUT='{"xgettext": {}}' mdbook build -d po
 msginit -i po/messages.pot -l zh -o po/zh.po --no-translator
 gpt-po-translator --lang zh --folder po/ --model deepseek-chat --bulk \
-									--no-ai-comment
+                  --no-ai-comment
